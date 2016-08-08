c9383e9de55f2b6cd3cec1f299d39ab12789457d
abkode <abozar.rahimi@gmail.com>
2016-08-08 19:09:39 +0000

    Models and Views has been created

--- app/actions.rb
+++ app/actions.rb
@@ -2,3 +2,32 @@
 get '/' do
   erb :index
 end
+
+get '/songs' do
+  @songs = Songs.all
+  erb :'songs/index'
+  # 'Message List here'
+end
+
+get '/songs/new' do
+  @songs = Songs.new
+  erb :'songs/new'
+end
+
+get '/songs/:id' do
+  @songs = Songs.find params[:id]
+  erb :'songs/show'
+end
+
+post '/songs' do
+  @songs = Songs.new(
+    song_title:   params[:song_title],
+    author: params[:author],
+    url:  params[:url]
+  )
+  if @songs.save
+    redirect '/songs'
+  else
+    erb :'songs/new'
+  end
+end
